a090895a61d01d972331ca0acbfca9340839ee34
walimike <walimike139@gmail.com>
2022-05-21 02:57:11 +0300

    add app layout

---
+++ app/rental/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
