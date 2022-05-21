79785d9e5704a5e717012e34012a306d127c1952
walimike <walimike139@gmail.com>
2022-05-21 05:42:26 +0300

    switch to postgres db

--- app/settings.py
+++ app/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'rental_app',
+       'USER': 'walimike',
+       'PASSWORD': '',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
