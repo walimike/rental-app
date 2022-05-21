8ba2c997c220dff358932d0e37de43d8ca8b47dc
walimike <walimike139@gmail.com>
2022-05-21 04:59:37 +0300

    clean up model

--- app/rental/models.py
+++ app/rental/models.py
@@ -13,16 +13,9 @@ class Reservations(models.Model):
     rental = models.ForeignKey(Rental, related_name='reservations', on_delete=models.CASCADE)
     checkin = models.DateTimeField(auto_now_add=True)
     checkout = models.DateTimeField(null=True, blank=True)
-    # previous_reservation = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.rental.name} reservation on {self.checkin}'
-
-    def save(self, *args, **kwargs):
-        import pdb
-        pdb.set_trace()
-        self.previous_reservation = Reservations.objects.filter().latest('id')
-        super(Reservations, self).save(*args, **kwargs)
 
     @property
     def rental_name(self):
@@ -31,4 +24,4 @@ class Reservations(models.Model):
     @property
     def previous_id(self):
         previous_item = self.rental.reservations.filter(id__lt=self.id)
-        return previous_item.latest('id').id if previous_item.exists() else None
+        return previous_item.latest('id').id if previous_item.exists() else '----'
